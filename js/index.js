d10a1b2ddda562d8bd607d80bbb51ca6407281ba
ceciliallee <chaelee@email.unc.edu>
2020-11-07 15:21:29 -0500

    button! and footer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
+// Load in animation
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("body");
     header.classList.add("loaded");
   });
+
+// Source card
+
+var button = document.getElementById('button-3'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('card');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};

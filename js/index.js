6d792ac5da3df1ecd2ea7fa6ac5e57919e1208d1
ceciliallee <chaelee@email.unc.edu>
2020-11-07 20:18:56 -0500

    slideshow, small edits

--- js/index.js
+++ js/index.js
@@ -17,3 +17,29 @@ button.onclick = function() {
         div.style.display = 'block';
     }
 };
+
+// Slideshow
+
+function slideShow(interval) {
+  let index = 0
+  const $imageEls = document.querySelectorAll('.container1 .slide') 
+  // this const fetches the images to cycle (the images in the container1)
+
+  setInterval(() => {
+    // this retrieves the next index and if it is at the end of the list, it resets to 0 (beginning of image "list")
+    index = index + 1 < $imageEls.length ? index + 1 : 0
+
+    // this retrieves the image at the index from ^
+    $imageEls[index].classList.add('show')
+	 
+    // this retrives the previous image, the one before the current index (index)
+    const previous = index - 1 < 0 ? $imageEls.length - 1 : index - 1;
+
+    // this removes the previous image (to display the next one)
+    $imageEls[previous].classList.remove('show')
+  }, interval)
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  slideShow(7500); // this insures the speed of the slideshow
+});
